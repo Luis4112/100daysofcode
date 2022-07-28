2d0f6216c7ec21fb597b6999d2d71d211fb7d51e
Luis4112 <54722424+Luis4112@users.noreply.github.com>
2022-07-28 21:29:02 +0100

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,11 @@
+class BST:
+    def __init___(self,key):
+        self.key = key,
+        self.lchild = None
+        self.rchild = None
+
+root = BST(5)
+print(root.key)
+print(root.lchild)
+print(root.rchild)
+
---
+++ outputonuser.py
@@ -0,0 +1,4 @@
+hey = {1,2,3,4,5}
+hey.add(5)
+hey.add(6)
+print(hey)
